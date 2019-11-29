76de46ae49b2cd5a0c6653f2bf57bad1c465bd55
KarenTatarov <karenatatarov@gmail.com>
2019-11-29 18:30:27 +0300

    Correct script and style

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 $(function() {
     $(document).ready(function() {
-        $.get('http://data.fixer.io/latest,
+        $.get('http://data.fixer.io/latest',
             {'access_key': 'f35e4b88c9c276fc9862312fa8939f02'},
             function(response) {
-                $('#usd').text(response.rates.RUB/response.rates.USD);
-                $('#euro').text(response.rates.RUB);
+                $('#usd').text(" = " + (response.rates.RUB/response.rates.USD).toFixed(2));
+                $('#euro').text(" = " + response.rates.RUB.toFixed(2));
             },
         );
     });
